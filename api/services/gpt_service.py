8a93db81e51a72a0a88320e6b9e6f6e79c0c4004
doeunyy <doeun990802@gmail.com>
2025-02-23 15:05:22 +0000

    feat: 기본 보정값 변경

--- api/services/gpt_service.py
+++ api/services/gpt_service.py
@@ -51,6 +51,7 @@ async def classify_function(text: str) -> str:
                             "- 프로필 사진 변경: change_profile_photo\n"
                             "- 이름 변경: change_name\n"
                             "- 비밀번호 변경: change_password\n"          
+                            "- 보정값 변경: adjust_correction_value\n"          
                             "- 로그아웃: logout\n"          
                             "- 탈퇴: quit\n"          
                             #! 여기에 계속 추가해야함 
